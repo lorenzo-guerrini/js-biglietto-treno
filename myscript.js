e1339c2e7197020b2bcc6e1d2e126c119a1893aa
lorenzo-guerrini <lorenzoguerrini8@live.it>
2021-12-03 15:46:29 +0100

    creati e strutturati index e style.css + funzione

--- myscript.js
+++ myscript.js
@@ -5,23 +5,27 @@
 // va applicato uno sconto del 40% per gli over 65.
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-const nKm = parseInt(prompt("Quanti km vuoi percorrere?"));
-const eta = parseInt(prompt("Quanti anni ha il passeggero?"));
+function priceCalculator() {
+    const nKm = parseInt(prompt("Quanti km vuoi percorrere?"));
+    const eta = parseInt(prompt("Quanti anni ha il passeggero?"));
 
-if (isNaN(nKm) || isNaN(eta)) {
-    console.log("Uno dei valori da te inseriti è scorretto. Ricomincia da capo.")
-    
-} else {
-    let prezzo = nKm * 0.21;
+    if (isNaN(nKm) || isNaN(eta)) {
+        document.getElementById("output").innerHTML = "Uno dei valori da te inseriti è scorretto. Ricomincia da capo."
 
-    if (eta < 18) {
-        prezzo -= prezzo * 20 / 100;
+    } else {
+        let prezzo = nKm * 0.21;
+
+        if (eta < 18) {
+            prezzo -= prezzo * 20 / 100;
+        }
+
+        if (eta >= 65) {
+            prezzo -= prezzo * 40 / 100;
+        }
+
+        prezzo = (Math.round(prezzo * 100) / 100).toFixed(2);
+
+        document.getElementById("price-container").style.display = "block";
+        document.getElementById("output").innerHTML = prezzo + "€";
     }
-
-    if (eta >= 65) {
-        prezzo -= prezzo * 40 / 100;
-    }
-
-    console.log(Math.round(prezzo * 100) / 100);
 }
-
